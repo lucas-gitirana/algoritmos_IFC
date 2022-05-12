8a1adb827e50953db34df6a47f0691754a7f0b60
Lucas Emanoel Gitirana <gitiranalucas5@gmail.com>
2022-05-12 09:29:47 -0300

    atualização do exercício

--- CON-SIMP-exe03.py
+++ CON-SIMP-exe03.py
@@ -2,14 +2,30 @@ a = int(input("A: "))
 b = int(input("B: "))
 c = int(input("C: "))
 
-if a > b and a > c:
+#if a > b and a > c:
+#    maior = a
+
+#if b > a and b > c:
+#    maior = b
+
+#if c > a and c > b:
+#    maior = c
+    
+maior = a
+
+if b > maior:
+    b = maior
+
+if c > maior:
+    c = maior  
+    
+# OU
+
+if a > b and a >c:
     maior = a
-
-if b > a and b > c:
+elif b > a and b > c:
     maior = b
-
-if c > a and c > b:
+else    
     maior = c
 
 print("O maior valor lido é: ", maior)
-
