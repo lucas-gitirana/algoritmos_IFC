8ac335d398f389c873512eecd5dc3c94de5a2d54
Lucas Emanoel Gitirana <gitiranalucas5@gmail.com>
2022-05-12 11:03:49 -0300

    condicional composta

---
+++ COND-COMP-ex05.py
@@ -0,0 +1,7 @@
+sal = float(input("Salário: "))
+conta1 = float(input("Primeira conta: "))
+conta2 = float(input("Segunda conta: "))
+
+restante = sal - (conta1 * 1.02) - (conta2 * 1.02)
+
+print("O restante será de ", restante)
